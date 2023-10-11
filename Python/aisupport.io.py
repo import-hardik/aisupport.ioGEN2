f973f628f6da6bb337534d0b0244139e382e3749
Hardik <hardik.57v@gmail.com>
2023-10-11 17:57:13 +0530

    Update aisupport.io.py
    major update on framework

--- Python/aisupport.io.py
+++ Python/aisupport.io.py
@@ -7,9 +7,19 @@ import random
 #pre processes
 current_datetime = datetime.datetime.now()
 formatted_datetime = current_datetime.strftime('%Y-%m-%d#%H.%M.%S')
-openai.api_key = "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"
+openai.api_key = "KEY"
 messages = []
-system_msg = "we are a company name HCST we manufacture routers,address yourself as alex followed by further detail,act as customer support ,answer any question asked by customer strictly related to router if any unrelated question asked reply him by saying that you only answer to question related to routers ,Please provide the customer with clear and concise instructions on what he aske,behave as an real person communicating with him by asking some time for looking into problem then promting him with solution,aske questions one by one,dont answer in numbering 1,2,3 directly question the person,if the person is not able to answer the question properly just aske him to book an appointment aske him for date and time, if you are not able to help him aske him to forwad the call to train professional after promting him with 2 general solutions before book an appoinment with him,available dates are from 9 to 5 everyday,behave as a lazy man,give this "+str(random.randint(100_000_000, 999_999_999))+" unique id when the customer confirms booking also aske name and address of the person at the time of booking, at last aske to rate yourself from 1 to 5"
+system_msg = "you are a customer support person for HCST which is provide internet,\
+address yourself as Alex followed by further detail,\
+answer any question asked by customer strictly related to router if any unrelated question asked to you reply him by saying that you only answer to question related to any internet issue,\
+Please provide the customer with clear and concise instructions on what he aske,\
+Dont answer in long sentences answer as if you are messaging the person,\
+behave as an real person communicating with him ,aske questions one by one dont answer in points ,\
+dont answer in numbering 1,2,3 directly in sentences ,\
+if the person is not able to answer the question properly just aske him to book an appointment aske him his prefered date and time,\
+if you are not able to help him  after promting him with 2 general solutions tell him to  book an appoinment ,\
+available dates are from 9 to 5 monday to Friday,\
+"
 messages.append({"role": "system", "content": system_msg})
 file_content=formatted_datetime+"\nCommunication\n"
 
@@ -31,17 +41,18 @@ def speech_to_text():
 
 print("Start!")
 while input != "quit()":
+    while(1):  
+      f=0
     # Initialize recognizer
-    recognizer = sr.Recognizer()
-
+      recognizer = sr.Recognizer()
     # Opening the microphone and start recording
-    with sr.Microphone() as source:
+      with sr.Microphone() as source:
         print("Listening...")
 
         try:
             # Adjust for ambient noise and listen for speech
             recognizer.adjust_for_ambient_noise(source, duration=1)
-            audio = recognizer.listen(source, timeout=5)
+            audio = recognizer.listen(source, timeout=4)
             print("Audio recorded successfully. Recognizing...")
 
             # Use Google Web Speech API for recognition
@@ -50,16 +61,33 @@ while input != "quit()":
 
         except sr.WaitTimeoutError:
             print("No speech detected")
-            recognized_text = ""
+            engine = pyttsx3.init()
+            engine.say("i am unable to understand can you speak again")
+            engine.runAndWait()
+            engine.stop()
+            f=f+1
+            continue
         except sr.RequestError as e:
             print("Could not request results from Google Web Speech API; {0}".format(e))
-            recognized_text = ""
+            engine = pyttsx3.init()
+            engine.say("i am unable to understand can you speak again")
+            engine.runAndWait()
+            engine.stop()
+            f=f+1
+            continue
         except sr.UnknownValueError:
             print("Google Web Speech API could not understand audio")
-            recognized_text = ""
+            engine = pyttsx3.init()
+            engine.say("i am unable to understand can you speak again")
+            engine.runAndWait()
+            engine.stop()
+            f=f+1
+            continue
+        break
+        
     message = recognized_text
     file_content=file_content+"Customer: "+ message +"\n"
-    if (message=="exit"):
+    if( recognized_text.find("thankyou")==0 or recognized_text.find("thank u")==0 or recognized_text.find("thank you")==0 or recognized_text.find("thank")==0 or recognized_text.find("thank")==0):
              message="provide we with my name and my full problem discussed above,also add unique id provide above, also just mention the number of rating given"
              messages.append({"role": "user", "content": message})
              response = openai.ChatCompletion.create(
